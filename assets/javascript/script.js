c4075c4f52e9efbd4b84902fd96065e22090a8ca
Pablo <pablomoreirasantos.hp@gmail.com>
2024-07-10 14:53:23 -0300

    feat: barra de pesquisa #06

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,26 +1,34 @@
-function submitForm() {
-  // Apenas teste 
-  window.location.href = 'dashboard.html';
-}
-
 const menuBtn = document.getElementById("menuBtn");
-const sidebar = document.getElementById("sidebar");
+const sidebar = document.querySelector(".sidebar");
 const overlay = document.getElementById("overlay");
-const userBtn = document.querySelector('.user');
+const userBtn = document.querySelector('.userBtn');
 const subMenu = document.querySelector('.sub-menu');
-const menuItems = document.querySelectorAll('.menu-item');
+const header = document.querySelector('header');
+const main = document.querySelector('main');
+const selecao = document.querySelector(".selecao");
+const texto = document.getElementById("texto");
+const opcoes = document.querySelectorAll(".opcao");
+const list = document.getElementById("list");
+const inputContainer = document.getElementById("inputContainer");
 
 // Função para alternar a visibilidade do sidebar
 menuBtn.onclick = function () {
   sidebar.classList.toggle("active");
-  overlay.classList.toggle("hidden");
+  header.classList.toggle("top");
+  main.classList.toggle("top");
+  if (overlay) {
+    overlay.classList.toggle("hidden");
+  }
 };
 
 // Função para esconder o sidebar ao clicar no overlay
-overlay.onclick = function () {
-  overlay.classList.toggle("hidden");
-  sidebar.classList.toggle("active");
-};
+if (overlay) {
+  overlay.onclick = function () {
+    overlay.classList.toggle("hidden");
+    sidebar.classList.toggle("active");
+    header.classList.toggle("shifted");
+  };
+}
 
 // Função para alternar a visibilidade do sub-menu
 function toggleSubMenu() {
@@ -28,27 +36,21 @@ function toggleSubMenu() {
 }
 
 // Adiciona o evento de clique ao botão de usuário
-userBtn.addEventListener('click', function(event) {
-  event.stopPropagation();
-  toggleSubMenu();
-});
+if (userBtn) {
+  userBtn.addEventListener('click', function (event) {
+    event.stopPropagation();
+    toggleSubMenu();
+  });
+}
 
 // Adiciona o evento de clique fora do sub-menu para fechá-lo
-document.addEventListener('click', function(event) {
-  if (!subMenu.contains(event.target) && !userBtn.contains(event.target)) {
-    subMenu.style.display = 'none';
-  }
-});
-
-// Inicialmente, esconde o sub-menu
-subMenu.style.display = 'none';
-
-// Adiciona um evento de clique a cada item do menu
-menuItems.forEach(item => {
-  item.addEventListener('click', function() {
-    // Remove a classe active de todos os itens do menu
-    menuItems.forEach(i => i.classList.remove('active-item'));
-    // Adiciona a classe active ao item clicado
-    this.classList.add('active-item');
+if (subMenu) {
+  document.addEventListener('click', function (event) {
+    if (!subMenu.contains(event.target) && !userBtn.contains(event.target)) {
+      subMenu.style.display = 'none';
+    }
   });
-});
+
+  // Inicialmente, esconde o sub-menu
+  subMenu.style.display = 'none';
+}
