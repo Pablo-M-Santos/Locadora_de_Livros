7c8295c6467fd789da99921ab37abb24d58f108f
Pablo <pablomoreirasantos.hp@gmail.com>
2024-07-12 14:48:39 -0300

    feat: criação dos modais cadastro e editar #08

--- assets/javascript/editora.js
+++ assets/javascript/editora.js
@@ -1,13 +1,21 @@
-const openModalButton = document.querySelector("#open-modal");
-const closeModalButton = document.querySelector("#close-modal");
-const modal = document.querySelector("#modal");
-const fade = document.querySelector("#fade");
+const modals = [
+  { open: "#open-modal1", close: "#close-modal1", modal: "#modal1", fade: "#fade1" },
+  { open: "#open-modal2", close: "#close-modal2", modal: "#modal2", fade: "#fade2" },
+  { open: "#open-modal3", close: "#close-modal3", modal: "#modal3", fade: "#fade3" }
+];
 
-const toggleModal = () => {
-  modal.classList.toggle("hide");
-  fade.classList.toggle("hide");
-};
+modals.forEach(({ open, close, modal, fade }) => {
+  const openModalButton = document.querySelector(open);
+  const closeModalButton = document.querySelector(close);
+  const modalElement = document.querySelector(modal);
+  const fadeElement = document.querySelector(fade);
 
-[openModalButton, closeModalButton, fade].forEach((el) => {
-  el.addEventListener("click", () => toggleModal());
+  const toggleModal = () => {
+    modalElement.classList.toggle("hide");
+    fadeElement.classList.toggle("hide");
+  };
+
+  [openModalButton, closeModalButton, fadeElement].forEach((el) => {
+    el.addEventListener("click", toggleModal);
+  });
 });
